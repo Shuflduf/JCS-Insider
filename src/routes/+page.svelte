ce4842c6b5e940407626a037a407e9800d55a53f
Shuflduf <shuflduf@shuflduf.xyz>
2025-03-20 15:39:46 -0600

    add api for frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { getMenu, type MenuData } from "./admin/appwrite";
+
+    let menu: MenuData = $state({});
+    onMount(async () => {
+        menu = await getMenu();
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#if menu}
+    <pre>{JSON.stringify(menu, null, 2)}</pre>
+{:else}
+    <p>Loading...</p>
+{/if}
