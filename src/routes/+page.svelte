81e8eab917322dd6ccb6032965ffcad3c2f398f5
FireEntity <134467282+FireEntity1@users.noreply.github.com>
2025-03-20 15:34:48 -0600

    nbjhsf;nbzjxfbkczf;bn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="justify-center">JCS Insider</h1>
+<p>Test</p>
