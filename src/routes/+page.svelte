1a2791b0b27fea0d142ece760ab225472015db44
Shuflduf <shuflduf@shuflduf.xyz>
2025-03-31 15:15:47 -0600

    funny svetle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
-    import { onMount } from "svelte";
     import { getMenu, type MenuData } from "./admin/appwrite";
 
-    let menu: MenuData = $state({});
-    onMount(async () => {
-        menu = await getMenu();
-    });
+    let menu: Promise<MenuData> = $state(getMenu());
 </script>
 
 <h1 class="justify-center">JCS Insider</h1>
 <p>Test</p>
 
-{#if menu}
-    <pre>{JSON.stringify(menu, null, 2)}</pre>
-{:else}
+{#await menu}
     <p>Loading...</p>
-{/if}
+{:then data}
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+{:catch error}
+    <p>{error.message}</p>
+{/await}
