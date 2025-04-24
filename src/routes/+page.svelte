705bbcc835e95eb99a5a4713805572a00e353860
Shuflduf <shuflduf@shuflduf.xyz>
2025-04-24 16:29:56 -0600

    asblru tdslikjskv

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,22 +17,23 @@
 {:then data}
     <p>{JSON.stringify(data)}</p>
 
-    <table>
-        <thead class="border-2"
-            ><tr>
-                <th class="border-2"> </th>
-                {#each Object.entries(data) as [key, item]}
-                    <th class="border-2"> {key} </th>
+    <div class="flex justify-evenly">
+        {#each Object.entries(data) as [weekday, dayData]}
+            <div
+                class="w-1/4 text-center text-3xl font-bold flex flex-col flex-wrap"
+            >
+                {weekday}
+                {#each Object.entries(dayData) as [categoryName, categoryData]}
+                    <p class="font-normal">{categoryName}</p>
+                    {#each Object.entries(categoryData) as [_, item]}
+                        <p class="font-normal text-base">
+                            {item.name} (${item.price})
+                        </p>
+                    {/each}
                 {/each}
-            </tr>
-
-            <tr>
-                {#each Object.entries(Object.entries(data)) as [key, item]}
-                    <td>{JSON.stringify(item[1].Main)}</td>
-                {/each}
-            </tr>
-        </thead>
-    </table>
+            </div>
+        {/each}
+    </div>
 {:catch error}
     <p>{error.message}</p>
 {/await}
--- src/routes/api/scraper/+server.ts
+++ src/routes/api/scraper/+server.ts
@@ -2,10 +2,72 @@ import fs from 'fs';
 import puppeteer from "puppeteer";
 import type { RequestHandler } from './$types';
 
+
 export const GET: RequestHandler = async (event) => {
     // Start a Puppeteer session with:
     // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
     // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    });
+
+    const page = await browser.newPage();
+    // const latestPostUrl = await getLatestPostUrl();
+    const latestPostUrl = "https://joanecardinalschubert.cbe.ab.ca/news/50218c72-8b11-41bf-8871-61d54f6d01d5"
+    await page.goto(latestPostUrl, {
+        waitUntil: "networkidle2",
+    });
+
+    await page.waitForSelector("#cravens-menu", { timeout: 5000 })
+    const result = await page.evaluate(() => {
+        const WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday"];
+        const menu = document.querySelector("#cravens-menu") as HTMLDivElement;
+        let nextSibling = menu.nextElementSibling as HTMLDivElement;
+        let siblings: HTMLDivElement[] = [];
+        let tree: any = {};
+        let currentWeekday: string = ""
+        while (nextSibling && nextSibling.tagName !== "DIV") {
+            WEEKDAYS.forEach(weekday => {
+                if (nextSibling.innerText.startsWith(weekday)) {
+                    currentWeekday = weekday
+                    currentWeekday.trim()
+                    tree[currentWeekday] = [];
+                    // tree[weekday] = nextSibling.innerText;
+                }
+            });
+            tree[currentWeekday]
+            siblings.push(nextSibling as HTMLDivElement);
+            nextSibling = nextSibling.nextElementSibling as HTMLDivElement;
+        }
+
+
+
+        return tree
+        // return siblings.map((sibling) => {
+        //     return sibling.innerText;
+        // })
+    });
+
+    console.log(result);
+
+    // const frames = await page.frames();
+
+    // console.log(frames);
+    // const eoriw = await frames[1].evaluate(() => {
+    //     const menu = document.querySelector("#cravens-menu");
+    //     return menu;
+    // })
+    // console.log(eoriw)
+
+
+    await browser.close();
+
+    return new Response({})
+};
+
+async function getLatestPostUrl(): Promise<string> {
+
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
@@ -27,37 +89,10 @@ export const GET: RequestHandler = async (event) => {
         });
 
     });
-    console.log(articles[0])
 
-    await page.goto(articles[0], {
-        waitUntil: "networkidle2"
-    })
-
-    await page.waitForSelector("#cravens-menu", { timeout: 5000 })
-
-    const frames = await page.frames();
-    const targetFrame = frames.find(frame => frame !== page.mainFrame());
-
-    if (!targetFrame) {
-        throw new Error('Could not find iframe');
-    }
-
-    const content = await targetFrame.evaluate(() => {
-        const editor = document.querySelector("#cravens-menu");
-        return editor ? editor.innerHTML : null;
-    });
-    console.log(content?.innerHTML)
-
-    const res = await page.evaluate(() => {
-        const foodMenu = document.querySelector("#cravens-menu")
-        return foodMenu
-    })
-    console.log(res?.className)
-
-
-    // Close the browser
     await browser.close();
 
-    return new Response({})
-};
+    console.log(articles[0]);
+    return articles[0] ?? ""
 
+}
