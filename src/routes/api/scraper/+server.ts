a48bab9b180aec79c23a537dea66840847be596b
Shuflduf <shuflduf@shuflduf.xyz>
2025-05-01 16:22:08 -0600

    6

--- src/routes/api/scraper/+server.ts
+++ src/routes/api/scraper/+server.ts
@@ -27,6 +27,7 @@ async function getLatestPostUrl(): Promise<string> {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
+        browser: 'firefox',
     });
 
     const page = await browser.newPage();
@@ -57,6 +58,7 @@ async function getLatestPostUrl(): Promise<string> {
 async function getBlogData(url: string): Promise<string[]> {
     const browser = await puppeteer.launch({
         headless: false,
+        browser: 'firefox',
         defaultViewport: null,
     });
 
