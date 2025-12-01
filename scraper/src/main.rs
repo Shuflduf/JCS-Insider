f3acdf34592e2bd201fd2ac7199257baae079dc8
Shuflduf <shuflduf@gmail.com>
2025-12-01 10:18:22 -0700

    chore(scraper): added scraper deps

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,3 +1,10 @@
+const NEWS_CENTRE_URL: &str = "https://joanecardinalschubert.cbe.ab.ca/news-centre";
+
 fn main() {
-    println!("Hello, world!");
+    let page_html = reqwest::blocking::get(NEWS_CENTRE_URL)
+        .unwrap()
+        .text()
+        .unwrap();
+    let page_content = scraper::Html::parse_document(&page_html);
+    println!("{page_content:?}");
 }
